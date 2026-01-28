4395a92e579e2c7e060a6336f1ef2a28cc1e520f
Darlei Mota <darleichans1@gmail.com>
2026-01-28 18:21:19 -0300

    chore(login.ts) adição de componetes do material para serem usados

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,19 @@
+
 import { Component } from '@angular/core';
+// compotentes do angular material
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button'
+import { M } from '@angular/cdk/keycodes';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule,
+     MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {
-
-}
+export class Login {}
